72e91c2464065f0b046cd1f6045c07c9a8bd8f19
StrayDragon <straydragonl@foxmail.com>
2019-01-10 16:06:18 +0800

    clean code

--- FrameworkSlice/Flutter/practice/library_crud_app/dev/library_app/lib/app/controller/book_controller.dart
+++
@@ -1,6 +0,0 @@
-import '../model/entity/book.dart';
-import '../model/dao/book_dao.dart';
-
-class BookController {
-  BookDao dao;
-}
--- FrameworkSlice/Flutter/practice/library_crud_app/dev/library_app/lib/app/model/dao/dao.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-
-abstract class Dao<M> {
-  Future<List<M>> fetchAll();
-
-  Future<bool> modifyInfoById(int uniqueId, Map<String, dynamic> updatingInfo);
-
-  Future<bool> removeById(int uniqueId);
-}
